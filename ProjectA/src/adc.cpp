d2b20f37b3dc9e82ddc4911323363e7299ac071c
MBorrageiro <mauro.borrageiro7@gmail.com>
2019-10-13 20:12:39 +0200

    Adc.h updated with constants

--- ProjectA/src/adc.cpp
+++ ProjectA/src/adc.cpp
@@ -1,5 +1,3 @@
-/*ADC testing in C*/
-
 #include "adc.h"
 #define BASE 777 //defines base for the chip of the analog pins
 #define VREF 3.3 //reference voltage
@@ -8,9 +6,8 @@
 using namespace std;
 
 int adcCh;
-//unsigned int Pot=0;
-//int mcp_answer;
 int reads [3];
+bool alarmErr = false;
 
 void setup_gpio(void){
     //Set up wiring Pi
@@ -21,28 +18,65 @@ void setup_gpio(void){
     mcp3004Setup(BASE, SPI_CHAN);
     printf("Set up success");
 }
-double tempConvert(int channel){
-    double Vout = voltageConversion(channel);
-    return (Vout-VZERO)/TC;
+
+double Vout(void){
+    /*Alarm detection variable*/
+    double vout =1;
+    vout =  (reads[1]/1023.0)*voltageConvert(0);
+    if(vout<0.65 || vout>2.65){
+	alarmErr = true; //alarm sound
+    }
+    else{
+	alarmErr = false; //operating stage is fine
+    }
+    return vout;
 }
 
-double voltageConversion(int channel){
+double tempConvert(int channel){
+    double Vadc = voltageConvert(channel);
+    return (Vadc-VZERO)/TC;
+}
+
+double voltageConvert(int channel){
     //Voltage conversion function
     return (VREF*reads[channel])/1024;
 }
 
-int main(){
-    setup_gpio();
-    printf("Reading values");
-    while(1){
+/*void *readAdcThread(void *threadarg){
+     printf("Testing if these run sim\n");
+     while(printDone){
 	for(adcCh=0; adcCh<3;adcCh++){
-	   // int port = BASE + adcCh;
+	    // int port = BASE + adcCh;
 	    reads[adcCh] = analogRead(BASE+adcCh);
 	    //read[adcCh] = pot;
+	    printf("%d",read[adcCh]);
 	}
-	//Pot = reads[0];
-	printf("Pot: %3.2f V\t Light sen: %d \t Temp: %3.2f *C\n", voltageConversion(0),reads[1],tempConvert(2));
-	for(int i = 0; i<10000000;i++);//voltageConversion(2));
+	printDone = !printDone;
     }
+    pthread_exit(NULL);
+}*/
 
+int main(){
+    setup_gpio();
+	///Thread///
+    /*pthread_attr_t tattr;
+    pthread_t thread_id;
+    int newprio = 99;
+    pthread_attr_init (&tattr);
+    sched_param param;
+    pthread_attr_getschedparam (&tattr, &param);
+    param.sched_priority = newprio; // set the priority; others are unchanged
+    pthread_attr_setschedparam (&tattr, &param); //setting the new scheduling param
+    pthread_create(&thread_id, &tattr, readAdcThread, (void *)1); // with new priority specified
+    pthread_join(thread_id, NULL);
+    printf("Reading values");*/
+    while(1){
+	for(adcCh=0; adcCh<3;adcCh++){
+	    reads[adcCh] = analogRead(BASE+adcCh);
+	}
+	printf("Pot: %3.2f V\t Light sen: %d \t Temp: %3.2f *C \t Vout: %3.2f\n", voltageConvert(0),reads[1],tempConvert(2),Vout());
+	for(int i = 0; i<10000000;i++);
+    }
+    //pthread_exit(NULL);
+    return 0;
 }
--- ProjectA/src/adc.h
+++ ProjectA/src/adc.h
@@ -14,27 +14,24 @@
 #include "mcp3004.h"
 
 //Define buttons I used WiPi
-#define PLAY_BUTTON 21 // Write your value here
-#define STOP_BUTTON 22 // Write your value here
+#define START_STOP_BTN 26 // Write your value here
+#define CHG_FREQ-BTN 6
+#define ALARM_RST_BTN 5
+#define RESET_BTN 13
+#define BASE 777
+#define VREF 3.3
+#define VZERO 0.5
+#define TC 0.01 //temperature co-efficient
 
 //SPI Settings
 #define SPI_CHAN 0 // Write your value here
 #define SPI_SPEED 1350000 // adc max clock for 3.3V
 
-//Filename
-//#define FILENAME "src/sound_16k_8bit.raw"
-
 //Function definitions
 void readAdc(int channel);
-/*void play_audio(void);
-void stop_audio(void);
-void stop_isr(void);
-void play_pause_isr(void);*/
-
 void setup_gpio(void);
 double tempConvert(int channel);
-double voltageConversion(int channel);
+double voltageConvert(int channel);
 int main(void);
-
 
 #endif
